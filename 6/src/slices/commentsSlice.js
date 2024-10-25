2ecf75b7135f4a3c435ae84476b4742be9d7162f
CheLaLuna <daniil20040815@gmail.com>
2024-10-25 12:52:52 +0300

    1

--- 6/src/slices/commentsSlice.js
+++ 6/src/slices/commentsSlice.js
@@ -11,11 +11,13 @@ const commentsSlice = createSlice({
   reducers: {
     setComments(state, { payload }) {
       // BEGIN (write your solution here)
-
+      state.ids = payload.map(comment => comment.id);
+      payload.forEach(comment => {
+        state.entities[comment.id] = comment;
       // END
-    },
+    });
   },
+},
 });
-
 export const { actions } = commentsSlice;
 export default commentsSlice.reducer;
